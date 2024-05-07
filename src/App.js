bb761156c478ee538d12981156c590efc5f419a6
Jay Duong <jayvin.duong@gmail.com>
2024-05-06 20:48:12 -0400

    Adjusting layout to account for smaller devices.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { SubjectLineList } from './components/SubjectLineList'
 
 function App() {
   return (
-    <div className="App relative">
+    <div className="App relative bg-[#1e1e1e]">
       <header className="App-header"></header>
 
-      <div className="grid grid-cols-2 h-dvh">
+      <div className="grid md:grid-cols-2 h-screen">
         <section className="col-span-1 bg-[#1e1e1e] p-[20px]">
           <SubjectLineList></SubjectLineList>
         </section>
 
-        <section className="col-span-1 fixed right-0 top-0 w-3/6 h-screen">
+        <section className="col-span-1 md:fixed md:right-0 md:top-0 md:w-3/6 md:h-screen">
           <SubjectLineForm></SubjectLineForm>
         </section>
       </div>
